c1c47b8739f8c61a091510548350ff0efe3922f2
Z1029 <87966654+Z1029@users.noreply.github.com>
2022-12-05 21:09:42 +0800

    自定义MetadataHeader顺序

---
+++ OZ_Il2cpp_Console/metadata_mgr.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <io.h>
+#include <vector>
+#include <Windows.h>
+#include <assert.h>
+#include <algorithm>
+#include <codecvt>
+#include "utils.h"
+#include "cpp_mgr.h"
+
+namespace metadata_mgr
+{
+};
+
--- OZ_Il2cpp_Console/src_res/mdc_init.cpp
+++ OZ_Il2cpp_Console/src_res/mdc_init.cpp
@@ -1,8 +1,8 @@
 /********************Start OZ Il2cpp Encryption********************/
     frontHeader = (FrontHeader *)s_GlobalMetadata;
-    char *Headerdata = (char *)malloc(frontHeader->legnth);
-    size_t Headerlen;
-    memcpy(Headerdata, (char*)s_GlobalMetadata + frontHeader->offset, frontHeader->legnth);
-    char *Header = (char *)xxtea_decrypt(Headerdata, frontHeader->legnth, frontHeader->key, &Headerlen);
-    s_GlobalMetadataHeader = (const Il2CppGlobalMetadataHeader*)Header;
+    char *headerdata = (char *)malloc(frontHeader->legnth);
+    size_t headerlen;
+    memcpy(headerdata, (char*)s_GlobalMetadata + frontHeader->offset, frontHeader->legnth);
+    char *header = (char *)xxtea_decrypt(headerdata, frontHeader->legnth, frontHeader->key, &headerlen);
+    s_GlobalMetadataHeader = (const Il2CppGlobalMetadataHeader*)header;
 /********************End OZ Il2cpp Encryption********************/
